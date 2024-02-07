b06e738047e737d10d5dd9f5d71b55162c732dae
Andy Delso <andy@delso.me>
2024-02-07 12:35:27 -0600

    improve docs a bit more for DriverFactories kmm

--- shared/src/commonMain/kotlin/DriverFactory.kt
+++ shared/src/commonMain/kotlin/DriverFactory.kt
@@ -4,8 +4,8 @@ import com.ddaypunk.aperture.db.ApertureDatabase
 /**
  * KMM expectation for platforms to implement their specific driver creation factories given the following:
  * - Database schema: [ApertureDatabase] (declared in :shared/build.gradle.kts sqldelight block) and *.sq files found in :shared/src/common/sqldelight
- * - in Android returns an AndroidSqliteDriver instance
- * - in iOS returns a NativeSqliteDriver instance
+ * - in Android returns an AndroidSqliteDriver instance - see :shared/src/androidMain/DriverFactory.android.kt
+ * - in iOS returns a NativeSqliteDriver instance - see :shared/src/iosMain/DriverFactory.ios.kt
  */
 expect class DriverFactory {
     fun createDriver(): SqlDriver
@@ -13,8 +13,7 @@ expect class DriverFactory {
 
 /**
  * KMM function to create the sqlite database using sqldelight library
- * @param driverFactory determined by client running the app currently
- * @see [DriverFactory] documentation
+ * @param driverFactory [DriverFactory] determined by client running the app currently via kmm internals
  * @return an instance of the database based on current platform
  */
 fun createDatabase(driverFactory: DriverFactory): ApertureDatabase {
