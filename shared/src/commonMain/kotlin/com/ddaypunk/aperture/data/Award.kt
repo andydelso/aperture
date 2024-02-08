c52f5184918d6ca1c611c38819c328fcd6e529ca
Andy Delso <andy@delso.me>
2024-02-07 21:06:24 -0600

    remove unneeded data classes

--- shared/src/commonMain/kotlin/com/ddaypunk/aperture/data/Award.kt
+++
@@ -1,7 +0,0 @@
-package com.ddaypunk.aperture.data
-
-data class Award(
-    val id: Int? = null,
-    val category: String,
-    val nominations: List<Nominee>
-)
--- shared/src/commonMain/kotlin/com/ddaypunk/aperture/data/Season.kt
+++
@@ -1,6 +0,0 @@
-package com.ddaypunk.aperture.data
-
-data class Season(
-    val year: Int,
-    val awards: List<Award>
-)
